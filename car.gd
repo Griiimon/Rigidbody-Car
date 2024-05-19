f30272ec008de65071d5f365a53b244edd6f6668
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-19 22:53:05 +0200

    first

--- car.gd
+++ car.gd
@@ -9,15 +9,22 @@ class_name Car
 
 
 @onready var front = $Front
+@onready var back = $Back
+
 @onready var default_friction= physics_material_override.friction
 @onready var default_linear_damp= linear_damp
+
+var is_on_ground= false
 
 
 
 func _physics_process(delta):
-	if Input.is_action_pressed("accelerate"):
+	if Input.is_action_pressed("accelerate") and is_on_ground:
 		# apply acceleration force to the front to stabilize car rotation
 		apply_force(get_forward() * acceleration, front.global_position - global_position)
+		#apply_force((get_forward() * acceleration + Vector3.UP * acceleration * 0.01), front.global_position - global_position)
+		apply_force(-global_basis.y * acceleration * 0.1, back.global_position - global_position)
+	
 	
 	if Input.is_action_pressed("brake"):
 		physics_material_override.friction= brake_friction
@@ -26,9 +33,11 @@ func _physics_process(delta):
 		physics_material_override.friction= lerp(1.0, default_friction, get_forward().dot(linear_velocity.normalized()))
 	
 	var steering= Input.get_axis("steer_left", "steer_right")
+	if not is_on_ground:
+		steering= 0
 	
 	# disable/reduce turning at no/very low speed
-	var adjusted_steer_force: float= lerp(0.0, steer_force, clamp(get_speed(), 0.0, 1.0))
+	var adjusted_steer_force: float= lerp(0.0, steer_force, clamp(get_speed() / 10.0, 0.0, 1.0))
 	apply_torque(Vector3.DOWN * steering * adjusted_steer_force)
 
 	# dynamically reduce linear damp at high speeds for more sliding
@@ -37,6 +46,13 @@ func _physics_process(delta):
 
 	DebugHud.send("Speed", int(get_speed()))
 
+
+func _integrate_forces(state):
+	is_on_ground= false
+	for col_idx in state.get_contact_count():
+		if state.get_contact_collider_object(col_idx).collision_layer == 2:
+			is_on_ground= true
+			break
 
 func get_forward()-> Vector3:
 	return -global_basis.z
